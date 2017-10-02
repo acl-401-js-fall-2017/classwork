fe946c5deea5ba47319453d5065b925ac7d283fd
martypdx <marty@martypdx.com>
2017-10-02 10:43:26 -0700

    passing getArgs test

---
+++ week01_JavaScript-and-Node-fundementals/class01_node-npm-modules/greeting-app/test/cli-args.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+function getArgs(args) {
+    return {
+        name: args[2],
+        foo: 'foo'
+    };
+}
+
+it('extracts name from argv', () => {
+    const options = getArgs(['node', 'greet.js', 'meow-meow']);
+    assert.deepEqual(options, { name: 'meow-meow' });
+});
