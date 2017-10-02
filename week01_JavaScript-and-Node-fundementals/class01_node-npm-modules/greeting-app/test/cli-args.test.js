5b5ab8a5608c0349dc32b482618cf39c252b741c
martypdx <marty@martypdx.com>
2017-10-02 10:51:02 -0700

    add describe blocks to name test suites

--- week01_JavaScript-and-Node-fundementals/class01_node-npm-modules/greeting-app/test/cli-args.test.js
+++ week01_JavaScript-and-Node-fundementals/class01_node-npm-modules/greeting-app/test/cli-args.test.js
@@ -1,13 +1,9 @@
 const assert = require('assert');
 const getArgs = require('../lib/cli-args');
 
-function getArgs(args) {
-    return {
-        name: args[2]
-    };
-}
-
-it('extracts name from argv', () => {
-    const options = getArgs(['node', 'greet.js', 'meow-meow']);
-    assert.deepEqual(options, { name: 'meow-meow' });
+describe('cli args', () => {     
+    it('extracts name from argv', () => {
+        const options = getArgs(['node', 'greet.js', 'meow-meow']);
+        assert.deepEqual(options, { name: 'meow-meow' });
+    });
 });
--- week01_JavaScript-and-Node-fundementals/class01_node-npm-modules/greeting-app/test/greeting.test.js
+++ week01_JavaScript-and-Node-fundementals/class01_node-npm-modules/greeting-app/test/greeting.test.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const greet = require('../lib/greet');
 
-it('greets with name', () => {
-    const greeting = greet('meow-meow');
-    assert.equal(greeting, 'hello meow-meow');
+describe('greeting', () => {
+    it('greets with name', () => {
+        const greeting = greet('meow-meow');
+        assert.equal(greeting, 'hello meow-meow');
+    });
 });
