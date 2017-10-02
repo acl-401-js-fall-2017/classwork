270dc6b592f6ea14f4242bc36c35345ccd90bbe6
martypdx <marty@martypdx.com>
2017-10-02 10:14:22 -0700

    easy-greet examples

---
+++ week01_JavaScript-and-Node-fundementals/class01_node-npm-modules/easy-greeting-app/greeting.js
@@ -0,0 +1,8 @@
+// 1. extract the name from the argv
+const name = process.argv[2];
+
+// 2. make greeting
+const greeting = 'hello ' + name;
+
+// 3. output the greeting to the console
+console.log(greeting);
