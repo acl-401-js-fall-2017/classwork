4ef741d5aa3ba0113d9e2058541c40d0c92757da
martypdx <marty@martypdx.com>
2017-10-18 15:31:08 -0700

    tdd cathentication example

--- week03_Express-and-Mongoose/class13_express-middleware/test/unit/cathentication.test.js
+++ week03_Express-and-Mongoose/class13_express-middleware/test/unit/cathentication.test.js
@@ -1,53 +1,42 @@
 const { assert } = require('chai');
+const createAuth = require('../../lib/utils/cathentication');
 
-function createAuth(password) {
-    return (req, res, next) => {
-        if(req.query.access_token !== password) {
-            res.status(401).send('user not cathenticated');
-        }
-        else next();
-    };
-}
-
-describe.only('cathentication', () => {
-
+describe('cathentication', () => {
     let ensureAuth = null;
     beforeEach(() => ensureAuth = createAuth('meow'));
 
-
     it('calls next when correct token is passed', () => {
         const req = { 
-            query: { access_token: 'meow' }
+            query: { access_token: 'meow' } 
         };
-
         let called = false;
         const next = () => called = true;
 
         ensureAuth(req, null, next);
-
+        
         assert.ok(called);
     });
 
     it('sends 401 when correct token not passed', () => {
         const req = { 
-            query: { access_token: 'woof' }
+            query: { access_token: 'woof' } 
         };
-        
+
         const res = {
-            status(code) {
+            status(code) { 
                 this.statusCode = code;
                 return this;
             },
-            send(message) {
-                this.message = message;
+            send(message) { 
+                this.message = message; 
             }
         };
 
         let called = false;
         const next = () => called = true;
-
+        
         ensureAuth(req, res, next);
-
+        
         assert.equal(res.statusCode, 401);
         assert.equal(res.message, 'user not cathenticated');
         assert.equal(called, false);
