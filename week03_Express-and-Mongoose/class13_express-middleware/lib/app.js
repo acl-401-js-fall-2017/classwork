46937b928a01b3c3a0d480f35202851ce55977de
martypdx <marty@martypdx.com>
2017-10-18 14:51:07 -0700

    TDD logger example

--- week03_Express-and-Mongoose/class13_express-middleware/lib/app.js
+++ week03_Express-and-Mongoose/class13_express-middleware/lib/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+// const morgan = require('morgan');
+const logger = require('./utils/logger');
+
+app.use(logger());
 
 app.use(express.static('./public'));
-app.use(bodyParser.json());
 
-const resource = require('./routes/resource');
-app.use('/api/resource', resource);
+app.use('/danger', () => {
+    throw new Error('You were warned!');
+});
+
+app.use('/foo', (req, res, next) => {
+    res.send('hello from app.use!');
+});
+
+
 
 module.exports = app;
