0641db50c5ab5df3dc4e7b9ec24bfb4c45e03efe
martypdx <marty@martypdx.com>
2017-11-07 16:53:42 -0800

    basic refetch based on user input

---
+++ week06_Frontend-SPA-React/class22_react-and-javascript/src/SWApp.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+export default class SWApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      resource: 'people',
+      loading: false
+    };
+  }
+
+  componentDidMount() {
+    this.loadResource(this.state.resource);
+  }
+
+  async loadResource(resource) {
+    this.setState({ loading: true });
+    const response = await fetch(`https://swapi.co/api/${resource}`);
+    const body = await response.json();
+    this.setState({
+      items: body.results,
+      loading: false
+    });
+  }
+
+  changeResource(resource) {
+    this.setState({ resource }, () => {
+      this.loadResource(resource);
+    });
+  }
+
+  render() {
+    const { resource, items, loading } = this.state;
+    const choices = ['people', 'planets', 'films', 'species', 'vehicles', 'starships'];
+    
+    const list = (
+      <ul>
+        {items.map(item => <li key={item.name}>{item.name}</li>)}
+      </ul>
+    );
+
+    const load = <div>Loading...</div>;
+
+    return (
+      <section>
+        {choices.map(choice => {
+          return <button key={choice} disabled={choice === resource}
+            onClick={() => this.changeResource(choice)}
+          >
+            {choice}
+          </button>;
+        })}
+        <div>{items.length} {resource}</div>
+        {loading ? load : list}
+      </section>
+    );
+  }
+}
--- week06_Frontend-SPA-React/class22_react-and-javascript/src/index.js
+++ week06_Frontend-SPA-React/class22_react-and-javascript/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import SWApp from './SWApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<SWApp/>, document.getElementById('root'));
 registerServiceWorker();
