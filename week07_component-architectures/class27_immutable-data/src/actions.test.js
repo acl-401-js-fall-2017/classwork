3f9d53ddf0443c982458702d57f472e6191628db
martypdx <marty@martypdx.com>
2017-11-14 18:09:49 -0800

    get rid of hacky cleanIds in favor of jest matcher

--- week07_component-architectures/class27_immutable-data/src/actions.test.js
+++ week07_component-architectures/class27_immutable-data/src/actions.test.js
@@ -1,38 +1,27 @@
-import React from 'react';
 import { addTodo, removeTodo, changeTodoCompletion } from './actions';
-
-function cleanIds(todos) {
-  todos.forEach(t => {
-    expect(t._id).toBeDefined();
-    delete t._id;
-  });
-}
 
 it('adds a todo to empty state todos', () => {
   const newState = addTodo({ todos: [] }, 'new todo');
-  cleanIds(newState.todos);
   expect(newState).toEqual({
     todos: [
-      { title: 'new todo', completed: false }
+      { _id: expect.any(String), title: 'new todo', completed: false }
     ]
   });
 });
 
 it('adds a todo and preservers existing state keys and todos', () => {
+  const existingTodo = { _id: '123abc', title: 'existing todo', completed: true }
   const state = { 
-    todos: [
-      { _id: 123, title: 'existing todo', completed: true }
-    ], 
-    name: 'foo' 
+    name: 'foo',
+    todos: [ existingTodo ]
   };
   const newState = addTodo(state, 'new todo');
-  cleanIds(newState.todos);
 
   expect(newState).toEqual({
     name: 'foo',
     todos: [
-      { title: 'existing todo', completed: true },
-      { title: 'new todo', completed: false }
+      existingTodo,
+      { _id: expect.any(String), title: 'new todo', completed: false }
     ]
   });
 });
