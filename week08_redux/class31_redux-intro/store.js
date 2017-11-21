6fface4b86110f40d09b37536c04d2455d605f3a
martypdx <marty@martypdx.com>
2017-11-20 17:06:09 -0800

    working pirates with redux and connect

---
+++ week08_redux/class31_redux-intro/store.js
@@ -0,0 +1,21 @@
+const { createStore } = require('redux');
+
+function countReducer(state = 0, { type }) {
+    switch(type) {
+    case 'INCREMENT':
+        return state + 1;
+    case 'DECREMENT':
+        return state - 1;
+    default: 
+        return state;
+    }
+}
+
+const store = createStore(countReducer);
+store.subscribe(() => {
+    console.log('store changed', store.getState());
+});
+
+store.dispatch({ type: 'DECREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
